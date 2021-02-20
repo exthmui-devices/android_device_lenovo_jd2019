ed7c796d42d9387a0a6fad3a994d3e6eca03eea8
Nauval Rizky <enuma.alrizky@gmail.com>
2021-02-20 18:57:03 +0000

    [SQUASH] jd2019: parts: Introduce raise to wake gesture

--- parts/src/org/lineageos/settings/doze/TiltSensor.java
+++ parts/src/org/lineageos/settings/doze/TiltSensor.java
@@ -23,6 +23,8 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.SystemClock;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
 
 import java.util.concurrent.ExecutorService;
@@ -34,6 +36,8 @@ public class TiltSensor implements SensorEventListener {
     private static final boolean DEBUG = false;
     private static final String TAG = "TiltSensor";
 
+    private static final int MIN_WAKEUP_INTERVAL_MS = 1000;
+    private static final int WAKELOCK_TIMEOUT_MS = 300;
     private static final int BATCH_LATENCY_IN_MS = 100;
     private static final int MIN_PULSE_INTERVAL_MS = 2500;
 
@@ -41,6 +45,11 @@ public class TiltSensor implements SensorEventListener {
     private Sensor mSensor;
     private Context mContext;
     private ExecutorService mExecutorService;
+    private PowerManager mPowerManager;
+    private WakeLock mWakeLock;
+
+    private Sensor mProximitySensor;
+    private boolean mInsidePocket = false;
 
     private long mEntryTimestamp;
 
@@ -49,6 +58,9 @@ public class TiltSensor implements SensorEventListener {
         mSensorManager = mContext.getSystemService(SensorManager.class);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
         mExecutorService = Executors.newSingleThreadExecutor();
+        mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
     }
 
     private Future<?> submit(Runnable runnable) {
@@ -57,17 +69,28 @@ public class TiltSensor implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        boolean isRaiseToWake = DozeUtils.isRaiseToWakeEnabled(mContext);
         if (DEBUG) Log.d(TAG, "Got sensor event: " + event.values[0]);
 
         long delta = SystemClock.elapsedRealtime() - mEntryTimestamp;
-        if (delta < MIN_PULSE_INTERVAL_MS) {
+        if (delta < (isRaiseToWake ? MIN_WAKEUP_INTERVAL_MS : MIN_PULSE_INTERVAL_MS)) {
             return;
         } else {
             mEntryTimestamp = SystemClock.elapsedRealtime();
         }
 
-        if (event.values[0] == 1) {
-            DozeUtils.launchDozePulse(mContext);
+        if (!isRaiseToWake && !DozeUtils.isPocketGestureEnabled(mContext)) {
+            mInsidePocket = false;
+        }
+
+        if (event.values[0] == 1 && !mInsidePocket) {
+            if (isRaiseToWake) {
+                mWakeLock.acquire(WAKELOCK_TIMEOUT_MS);
+                mPowerManager.wakeUp(SystemClock.uptimeMillis(),
+                    PowerManager.WAKE_REASON_GESTURE, TAG);
+            } else {
+                DozeUtils.launchDozePulse(mContext);
+            }
         }
     }
 
@@ -85,10 +108,25 @@ public class TiltSensor implements SensorEventListener {
         });
     }
 
+    private SensorEventListener mProximityListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            mInsidePocket = event.values[0] < mProximitySensor.getMaximumRange();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // stub
+        }
+    };
+
     protected void disable() {
         if (DEBUG) Log.d(TAG, "Disabling");
         submit(() -> {
             mSensorManager.unregisterListener(this, mSensor);
+            if (DozeUtils.isRaiseToWakeEnabled(mContext)) {
+                mSensorManager.unregisterListener(mProximityListener, mProximitySensor);
+            }
         });
     }
 }
