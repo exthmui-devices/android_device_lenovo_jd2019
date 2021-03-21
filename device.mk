faca1b22ee3c2d705d6f03c80559024570018bcd
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-03-21 15:42:32 +0100

    jd2019: grant permissions for carrier settings
    java.lang.IllegalStateException: Signature|privileged permissions not in privapp-permissions whitelist: {com.google.android.carrier (/product/priv-app/CarrierSettings): android.permission.WRITE_APN_SETTINGS, com.google.android.carrier (/product/priv-app/CarrierSettings): android.permission.CHANGE_COMPONENT_ENABLED_STATE, com.google.android.carrier (/product/priv-app/CarrierSettings): android.permission.INTERACT_ACROSS_USERS, com.google.android.carrier (/product/priv-app/CarrierSettings): android.permission.MODIFY_PHONE_STATE, com.google.android.carrier (/product/priv-app/CarrierSettings): android.permission.READ_PRIVILEGED_PHONE_STATE, com.google.android.carrier (/product/priv-app/CarrierSettings): com.android.permission.INSTALL_EXISTING_PACKAGES}

--- device.mk
+++ device.mk
@@ -31,7 +31,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-sakura
 
 # Permissions
 PRODUCT_COPY_FILES += \
-   frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
+   frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
+   $(LOCAL_PATH)/configs/google-hiddenapi-package-whitelist-carriersettings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-hiddenapi-package-whitelist-carriersettings.xml \
+   $(LOCAL_PATH)/permissions/privapp-permissions-google-carriersettings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-carriersettings.xml
 
 # Audio
 PRODUCT_PACKAGES += \
